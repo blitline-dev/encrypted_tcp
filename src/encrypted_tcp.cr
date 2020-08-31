57e04a9ecafe42dfbe9c4f83609168e66bba37f1
Jason <jaciones@gmail.com>
2020-08-30 17:44:18 -0700

    Removed encoding temporarily

--- src/encrypted_tcp.cr
+++ src/encrypted_tcp.cr
@@ -6,5 +6,5 @@ require "base64"
 require "benchmark"
 
 module EncryptedTcp
-  VERSION = "0.1.16"
+  VERSION = "0.1.17"
 end
--- src/shared/encryptor.cr
+++ src/shared/encryptor.cr
@@ -1,11 +1,11 @@
-require "cox"
+# require "cox"
 require "base64"
 
 class EncryptedTcp::Encryptor
   def initialize(local_secret_key : String, local_public_key : String, remote_public_key : String)
-    @local_secret_key = Cox::SecretKey.new(Base64.decode(local_secret_key.strip))
-    @local_public_key = Cox::PublicKey.new(Base64.decode(local_public_key.strip))
-    @remote_public_key = Cox::PublicKey.new(Base64.decode(remote_public_key.strip))
+    # @local_secret_key = Cox::SecretKey.new(Base64.decode(local_secret_key.strip))
+    # @local_public_key = Cox::PublicKey.new(Base64.decode(local_public_key.strip))
+    # @remote_public_key = Cox::PublicKey.new(Base64.decode(remote_public_key.strip))
   end
 
   def self.generate_keypair
@@ -13,16 +13,19 @@ class EncryptedTcp::Encryptor
   end
 
   def encrypt(data)
+    return Base64.urlsafe_encode(daata)
     # Encrypt a message for Bob using his public key, signing it with Alice's
-    nonce, encrypted = Cox.encrypt(data, @remote_public_key, @local_secret_key)
-    return "#{Base64.urlsafe_encode(nonce.bytes, false)}:#{Base64.urlsafe_encode(encrypted, false)}"
+    #   nonce, encrypted = Cox.encrypt(data, @remote_public_key, @local_secret_key)
+
+    #  return "#{Base64.urlsafe_encode(nonce.bytes, false)}:#{Base64.urlsafe_encode(encrypted, false)}"
   end
 
   def decrypt(data)
-    if data
-      nonce, raw = data.split(":")
-      decrypted = Cox.decrypt(Base64.decode(raw), Cox::Nonce.new(Base64.decode(nonce)), @remote_public_key, @local_secret_key)
-      String.new(decrypted)
-    end
+    return Base64.decode(data)
+    # if data
+    #   nonce, raw = data.split(":")
+    #   decrypted = Cox.decrypt(Base64.decode(raw), Cox::Nonce.new(Base64.decode(nonce)), @remote_public_key, @local_secret_key)
+    #   String.new(decrypted)
+    # end
   end
 end
